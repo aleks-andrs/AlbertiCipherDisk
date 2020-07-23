f11736f2c1affcf5a605788e49755b1b3847c398
aleks-andrs <0602.id@gmail.com>
2020-07-23 01:03:11 +0100

    Partially completed encoder

--- src/app/encoder/encoder.component.ts
+++ src/app/encoder/encoder.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlberticipherService } from './../services/alberticipher.service';
 
 @Component({
   selector: 'app-encoder',
@@ -6,15 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./encoder.component.css']
 })
 export class EncoderComponent implements OnInit {
+  textInput: string;
 
-  constructor() { }
+  constructor(
+    private albertiCipherService: AlberticipherService
+  ) { }
 
   ngOnInit(): void {
 
   }
 
   onEncode(){
+    //perform a text validation
 
+
+    //process password and add rotation commands
+    var extendedPlainText = this.albertiCipherService.addRotationCommands(this.textInput);
+    //encode the plaintext and return ciphertext
+    var cipherText = this.albertiCipherService.encode(extendedPlainText);
+    if(cipherText){
+      alert(cipherText);
+    }else{
+      alert("Encoding error!");
+    }
 
   }
 }
--- src/app/services/alberticipher.service.ts
+++ src/app/services/alberticipher.service.ts
@@ -4,14 +4,16 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AlberticipherService {
+
   //plain text symbols and commands
-  textSymbols = ["A1" ,"A2" ,"A3" ,"A4" ,"A5" ,"A6" ," " ,"!" ,"\"" ,"#" ,
-  "$" ,"%" ,"&" ,"'" ,"(" ,")" ,"*" ,"+" ,"," ,"-" ,"." ,"/" ,"0" ,"1" ,"2" ,
-  "3" ,"4" ,"5" ,"6" ,"7" ,"8" ,"9" ,":" ,";" ,"<" ,"=" ,">" ,"?" ,"@" ,"A" ,
-  "B" ,"C" ,"D" ,"E" ,"F" ,"G" ,"H" ,"I" ,"J" ,"K" ,"L" ,"M" ,"N" ,"O" ,"P" ,
-  "Q" ,"R" ,"S" ,"T" ,"U" ,"V" ,"W" ,"X" ,"Y" ,"Z" ,"[" ,"\\" ,"]" ,"^" ,"_" ,
-  "`" ,"a" ,"b" ,"c" ,"d" ,"e" ,"f" ,"g" ,"h" ,"i" ,"j" ,"k" ,"l" ,"m" ,"n" ,
-  "o" ,"p" ,"q" ,"r" ,"s" ,"t" ,"u" ,"v" ,"w" ,"x" ,"y" ,"z" ,"{" ,"|" ,"}" ,"~"];
+  textSymbols = [" " ,"!" ,"\"" ,"#" , "$" ,"%" ,"&" ,"'" ,"(" ,")" ,"*" ,
+  "+" ,"," ,"-" ,"." ,"/" ,"0" ,"1" ,"2" , "3" ,"4" ,"5" ,"6" ,"7" ,"8" ,
+  "9" ,":" ,";" ,"<" ,"=" ,">" ,"?" ,"@" ,"A" , "B" ,"C" ,"D" ,"E" ,"F" ,
+  "G" ,"H" ,"I" ,"J" ,"K" ,"L" ,"M" ,"N" ,"O" ,"P" , "Q" ,"R" ,"S" ,"T" ,
+  "U" ,"V" ,"W" ,"X" ,"Y" ,"Z" ,"[" ,"\\" ,"]" ,"^" ,"_" , "`" ,"a" ,"b" ,
+  "c" ,"d" ,"e" ,"f" ,"g" ,"h" ,"i" ,"j" ,"k" ,"l" ,"m" ,"n" , "o" ,"p" ,
+  "q" ,"r" ,"s" ,"t" ,"u" ,"v" ,"w" ,"x" ,"y" ,"z" ,"{" ,"|" ,"}" ,"~" ,
+  "Ā" ,"Ē" ,"Ī" ,"Ō" ,"Ū" ,"Ĥ"];
 
   //cipher text symbols
   cipherTextSymbols = ["u" ,"0" ,"t" ,"K" ,"e" ,"g" ,"[" ,"X" ,"~" ,"ć" ,
@@ -22,25 +24,115 @@ export class AlberticipherService {
   "," ,"3" ,"O" ,"!" ,"z" ,"j" ,"A" ,"7" ,"J" ,"Õ" ,"-" ,"*" ,"8" ,"{" ,"H" ,
   "V" ,"x" ,"m" ,"$" ,"`" ,"_" ,"L" ,"=" ,">" ,"+" ,"Á" ,"C" ,"Y" ,"4" ,"'" ,"M"];
 
+  //rotation command characters
+  rotationCommandChars = "ĀĒĪŌŪĤ";
+
+  //rotation indexes
+  Ā: number = 1;
+  Ē: number = 2;
+  Ī: number = 3;
+  Ō: number = 4;
+  Ū: number = 5;
+  Ĥ: number = 6;
+
+  //rotation direction
+  isRight: Boolean = true;
+
   constructor() { }
 
+  addRotationCommands(text){
+    var newText = "";
+    var i = 0;
+    while(i<text.length){
+      //randomly add a rotation command to original plaintext
+      var randomBoolean = Math.random() >= 0.66;
+      if(randomBoolean){
+        newText = newText + this.rotationCommandChars.charAt(Math.floor(Math.random() * 6)) + text.charAt(i);
+      }else{
+        newText = newText + text.charAt(i);
+      }
+      i++;
+    }
+    return newText;
+  }
+
+
   shiftLeft(){
-    //shift symbols to the left
-    var i=0;
-    while(i<1){
-      var firstElement = cipherTextSymbols.shift();
-      cipherTextSymbols.push(firstElement);
+    //shift disk to the left
+    var i = 0;
+    while(i < 1){
+      var firstElement = this.cipherTextSymbols.shift();
+      this.cipherTextSymbols.push(firstElement);
       i++;
     }
   }
 
+
   shiftRight(){
-    //shift symbols to the right
-    var i=0;
-    while(i<1){
-      var lastElement = cipherTextSymbols.pop();
-      cipherTextSymbols.unshift(lastElement);
+    //shift disk to the right
+    var i = 0;
+    while(i < 1){
+      var lastElement = this.cipherTextSymbols.pop();
+      this.cipherTextSymbols.unshift(lastElement);
       i++;
     }
+  }
+
+  shiftDisk(rotationIndex){
+    var i = 0;
+    //rotate left or right using preset rotationIndexes
+    if(this.isRight){
+      while(i < rotationIndex){
+        this.shiftRight();
+        i++;
+      }
+    }else{
+      while(i < rotationIndex){
+        this.shiftLeft();
+        i++;
+      }
+    }
+  }
+
+  encode(text){
+    var cipherText = "";
+    var i = 0;
+    while(i < text.length){
+      if(this.textSymbols.includes(text.charAt(i))){
+        //set initial disk position in here
+        
+        //try hashsum?
+
+
+
+
+        if(this.rotationCommandChars.includes(text.charAt(i))){
+          var cipherText = cipherText + this.cipherTextSymbols[this.textSymbols.indexOf(text.charAt(i))];
+          var rotationPosititions = 1;
+          switch(text.charAt(i)){
+            case "Ā": rotationPosititions = this.Ā;
+            break;
+            case "Ē": rotationPosititions = this.Ē;
+            break;
+            case "Ī": rotationPosititions = this.Ī;
+            break;
+            case "Ō": rotationPosititions = this.Ō;
+            break;
+            case "Ū": rotationPosititions = this.Ū;
+            break;
+            case "Ĥ": rotationPosititions = this.Ĥ;
+            break;
+          }
+          this.shiftDisk(rotationPosititions);
+        }else{
+          var cipherText = cipherText + this.cipherTextSymbols[this.textSymbols.indexOf(text.charAt(i))];
+        }
+      }
+      else{
+        return null;
+      }
+      i++;
+    }
+    return cipherText;
   }
 }
